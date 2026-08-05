62896a79aa7bc06acefee0b28df5d246f6f760c2
liamlin-ship-it <liamlin9606@gmail.com>
2026-08-05 15:47:23 +0800

    Time: 28 ms (48.98%) | Memory: 47.1 MB (64.73%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeDuplicates(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && stack.peek() == (s.charAt(i))){
+                stack.pop();
+            }else {
+                stack.push(s.charAt(i));
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : stack){
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
