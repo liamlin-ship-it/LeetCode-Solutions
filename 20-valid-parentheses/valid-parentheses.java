cc8c139e4504dcb4eff40c325b933cc889bdb10f
liamlin-ship-it <liamlin9606@gmail.com>
2026-08-19 14:35:11 +0800

    Time: 3 ms (86.09%) | Memory: 43.4 MB (41.34%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,16 +3,16 @@ class Solution {
         Deque<Character> stack = new ArrayDeque<>();
 
             for (char c : s.toCharArray()){
-                if (c == '(' || c == '{' || c == '['){
-                    stack.push(c);
-                }else {
-                    if (stack.isEmpty()) return false;
-
-                    char top = stack.pop();
-
-                    if (c == ')' && top != '(') return false;
-                    if (c == '}' && top != '{') return false;
-                    if (c == ']' && top != '[') return false;
+                if (c == '(') {
+                    stack.push(')');
+                } else if (c == '{') {
+                    stack.push('}');
+                } else if (c == '[') {
+                    stack.push(']');
+                } else {
+                    if (stack.isEmpty() || stack.pop() != c){
+                        return false;
+                    }
                 }
             }
 
