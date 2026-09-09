6ac229b9566a09901cd4c0b15bcc639d2137ce79
liamlin-ship-it <liamlin9606@gmail.com>
2026-09-09 14:08:07 +0800

    Time: 6 ms (87.19%) | Memory: 45.2 MB (78.63%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -5,12 +5,12 @@ class Solution {
         for (String token : tokens){
             if (token.equals("+")){
                 stack.push(stack.pop() + stack.pop());
+            } else if (token.equals("*")){
+                stack.push(stack.pop() * stack.pop());
             } else if (token.equals("-")) {
                 int a = stack.pop();
                 int b = stack.pop();
                 stack.push(b - a);
-            } else if (token.equals("*")) {
-                stack.push(stack.pop() * stack.pop());
             } else if (token.equals("/")) {
                 int a = stack.pop();
                 int b = stack.pop();
@@ -19,7 +19,7 @@ class Solution {
                 stack.push(Integer.parseInt(token));
             }
         }
-
+        
         return stack.pop();
     }
 }
